b61b8d2ba03b8fe525e7e399d81ea9eb174f1f79
montaj <sekhmontajulali1997@gmail.com>
2023-07-06 19:43:18 +0530

    all file

---
+++ app/data.js
@@ -0,0 +1,55 @@
+/* clasic home page data design */
+
+// slider section
+const hSlider = [
+    {
+        id:1,
+        bImage: 'https://t4.ftcdn.net/jpg/02/18/18/55/360_F_218185587_P4zituDtWJOfClUKL6merI0BgLMIxoeC.jpg',
+        fTitle: ' DO YOU NEED A NEW ',
+        sndTitle: ' WEB DESIGN? ',
+        thrdTitle: ' Check out our options and features ',
+
+    },
+    {
+        id:2,
+        bImage: 'https://t4.ftcdn.net/jpg/02/18/18/55/360_F_218185587_P4zituDtWJOfClUKL6merI0BgLMIxoeC.jpg',
+        fTitle: '  WE WORK HARD AND PORTO HAS  ',
+        sndTitle: ' THE BEST DESIGN',
+        thrdTitle: ' Trusted by over 40,000 satisfied users, Porto is a huge success in the one of largest world\'s MarketPlace ',
+
+    },
+    {
+        id:3,
+        bImage: 'https://t4.ftcdn.net/jpg/02/18/18/55/360_F_218185587_P4zituDtWJOfClUKL6merI0BgLMIxoeC.jpg',
+        fTitle: ' DO YOU NEED A NEW ',
+        sndTitle: ' WEB DESIGN? ',
+        thrdTitle: ' Check out our options and features ',
+
+    },
+    {
+        id:4,
+        bImage: 'https://t4.ftcdn.net/jpg/02/18/18/55/360_F_218185587_P4zituDtWJOfClUKL6merI0BgLMIxoeC.jpg',
+        fTitle: ' DO YOU NEED A NEW ',
+        sndTitle: ' WEB DESIGN? ',
+        thrdTitle: ' Check out our options and features ',
+
+    },
+    {
+        id:5,
+        bImage: 'https://t4.ftcdn.net/jpg/02/18/18/55/360_F_218185587_P4zituDtWJOfClUKL6merI0BgLMIxoeC.jpgg',
+        fTitle: ' DO YOU NEED A NEW ',
+        sndTitle: ' WEB DESIGN? ',
+        thrdTitle: ' Check out our options and features ',
+
+    },
+    {
+        id:6,
+        bImage: 'https://t4.ftcdn.net/jpg/02/18/18/55/360_F_218185587_P4zituDtWJOfClUKL6merI0BgLMIxoeC.jpg',
+        fTitle: ' DO YOU NEED A NEW ',
+        sndTitle: ' WEB DESIGN? ',
+        thrdTitle: ' Check out our options and features ',
+
+    },
+];
+
+// team page data
